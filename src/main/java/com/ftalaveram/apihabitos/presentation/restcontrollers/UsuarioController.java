4a7d01d1f382a020ad60f98111a311903fb2c320
Fernando Talavera <ftalaveram01@gmail.com>
2025-02-13 19:27:14 +0100

    Warnings corregidos

--- src/main/java/com/ftalaveram/apihabitos/presentation/restcontrollers/UsuarioController.java
+++ src/main/java/com/ftalaveram/apihabitos/presentation/restcontrollers/UsuarioController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ftalaveram.apihabitos.business.model.Habito;
 import com.ftalaveram.apihabitos.business.model.Usuario;
 import com.ftalaveram.apihabitos.business.services.UsuarioServices;
 
